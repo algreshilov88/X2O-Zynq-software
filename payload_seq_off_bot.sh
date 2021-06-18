b31ae2f9053d7ec4c56acdc8cff9480df1b24e43
root <root@localhost.localdomain>
2021-06-18 01:03:06 +0000

    payload power scripts

---
+++ payload_seq_off_bot.sh
@@ -0,0 +1,6 @@
+b=0
+i2cset -y $b 0x71 0 # disconnect all
+i2cset -y $b 0x71 1 # enable expander reg branch
+#payload sequence off 
+i2cset -y $b 0x45 0x24 0x00
+
--- payload_sequence_off.sh
+++
@@ -1,5 +0,0 @@
-i2cset -y 1 0x71 0 # disconnect all
-i2cset -y 1 0x71 1 # enable expander reg branch
-#payload sequence off 
-i2cset -y 1 0x45 0x24 0x00
-
