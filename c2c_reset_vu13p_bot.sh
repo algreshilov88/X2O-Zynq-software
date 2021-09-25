de0c41bd3e0f1a9fd47139412ffdfaef34f3d8b6
Alexander Madorsky <16050565+madorskya@users.noreply.github.com>
2021-09-25 18:59:52 -0400

    removed more generated files

---
+++ c2c_reset_vu13p_bot.sh
@@ -0,0 +1,8 @@
+# all TX signals inverted on power board
+./devreg.sh tx_polarity 0x0
+./devreg.sh channel_up_bot 0
+./devreg.sh c2c_slave_reset_bot 1
+./devreg.sh c2c_slave_reset_bot 0
+./devreg.sh channel_up_bot 1
+./devreg.sh link_stat_bot
+
---
+++ c2c_reset_vu13p_top.sh
@@ -0,0 +1,8 @@
+# all TX signals inverted on power board
+./devreg.sh tx_polarity 0x0
+./devreg.sh channel_up_top 0
+./devreg.sh c2c_slave_reset_top 1
+./devreg.sh c2c_slave_reset_top 0
+./devreg.sh channel_up_top 1
+./devreg.sh link_stat_top
+
