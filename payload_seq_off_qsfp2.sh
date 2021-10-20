434b687a96f4577935dc99d4ad3f9266f36cb3d7
Alexander Madorsky <16050565+madorskya@users.noreply.github.com>
2021-10-20 17:47:03 -0400

    more qsfp scripts

---
+++ payload_seq_off_qsfp2.sh
@@ -0,0 +1,11 @@
+b=2
+
+# disable POK change reaction
+./devreg.sh pok_change_enable_qsfp 0
+
+#payload sequence off 
+i2cset -y $b 0x10 0xfe 0x09 0x21 i
+
+# reset interrupt, by reading interrupt status. The status is in MSB
+#i2cget -y $b 0x45 0x06
+
---
+++ payload_seq_on_qsfp2.sh
@@ -0,0 +1,21 @@
+b=2
+
+# disable POK change reaction
+./devreg.sh pok_change_enable_qsfp 0
+
+# payload sequence on 
+i2cset -y $b 0x10 0xff 0x09 0x21 i
+
+# let power turn on
+#sleep 0.2
+
+# check POK bits from payload
+#i2cget -y $b 0x45 0x58
+
+# enable register and interrupt output P31
+#i2cset -y $b 0x45 0x04 0x81
+
+#enable POK change reaction
+#./devreg.sh pok_change_enable_bot 1
+
+
